d4937be2417c2271da79ace8ca3e75f43efbe8c3
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-19 08:28:08 +0600

    27-6 verify pin number and add money to the balance

--- js/home.js
+++ js/home.js
@@ -1,1 +1,48 @@
-console.log('taka.....');
+// console.log('taka.....');
+
+//add money to the account
+//step-1: add an event handler to the add money button inside the form
+
+//step-1: add event handler and prevent page reload after form submit
+//step-2: get money to be added to the account balance.
+//step-3: verify pin number 
+//wrong way to pin validate
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+
+	//prevent page reload after form submit
+	event.preventDefault();
+	// console.log('clicked');
+
+	//step-2: get money to be added to the account balance
+	const addMoneyInput = document.getElementById('input-add-money').value;
+	// console.log(addMoneyInput);
+
+	//step-3: get the pin number provided
+	const pinNumberInput = document.getElementById('input-pin-number').value;
+	// console.log(pinNumberInput);
+
+	//step-3: verify pin number 
+	//wrong way to validate pin number
+	if(pinNumberInput === '1234'){
+		console.log('adding money to your account');
+
+		//step-4: get the current balance
+		const balance = document.getElementById('account-balance').innerText;
+		// console.log(balance);
+
+		//step-5: add addMoneyInput with balance
+		const addMoneyNumber = parseFloat(addMoneyInput);
+		const balanceNumber = parseFloat(balance);
+		const newBalance = addMoneyNumber + balanceNumber;
+
+
+		console.log(newBalance);
+
+
+		//step-6: update the balance in the ui/dom
+		document.getElementById('account-balance').innerText = newBalance;
+	}else{
+		alert('Failed to add money! please try again');
+	}
+})
